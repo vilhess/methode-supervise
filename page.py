29e956f298a5f18034ab778f5d8bf52b6c40a484
vilhess <samy.vilhes@gmail.com>
2023-10-11 09:35:55 +0200

    amelioration

--- page.py
+++ page.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.decomposition import PCA
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+from sklearn.metrics import r2_score, accuracy_score, confusion_matrix, classification_report
 
 st.markdown(f"<h1 style='text-align: center;'>Projet Supervisé Avancé</h1>", unsafe_allow_html=True)
 st.subheader('')
@@ -37,7 +39,7 @@ if probleme == 'Régression':
     corr = df_train_reg.corr()
 
     fig = plt.figure(figsize=(10, 10))
-    sns.heatmap(corr, annot=True)
+    sns.heatmap(corr, annot=True, cmap='coolwarm')
     st.pyplot(fig)
 
     st.subheader('')
@@ -76,17 +78,11 @@ if probleme == 'Régression':
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Plans factoriels</h5>", unsafe_allow_html=True)
-
-    st.subheader('')
-
-    st.pyplot(fig)
-
     plan_principaux = st.sidebar.slider('Plan factoriel à afficher', 1, 10, 1)
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Projection des individus sur le plan factoriel {plan_principaux}</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Projection des individus sur le plan factoriel {plan_principaux}, {plan_principaux+1} </h5>", unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -100,6 +96,26 @@ if probleme == 'Régression':
     plt.ylabel('PC' + str(plan_principaux + 1))
     plt.grid()
     st.pyplot(fig)
+
+    st.subheader('')
+
+    st.markdown(f"<h5 style='text-align: center;'>Résultats de nos modèles</h5>", unsafe_allow_html=True)
+
+    st.subheader('')
+
+    # load every dataframe in the folder résultats_models/regression
+    path = 'résultats_models/regression'
+    files = os.listdir(path)
+    files.sort()
+    files = [file for file in files if file.endswith('.csv')]
+
+    df = pd.DataFrame({'Modèle': [], 'R2': []})
+
+    for file in files:
+        df_temp = pd.read_csv(os.path.join(path, file))
+        df = df._append({'Modèle': file.split('.')[0].replace('_', ' '), 'R2': r2_score(df_temp['y_test'], df_temp['y_pred'])}, ignore_index=True)
+
+    st.table(df.sort_values(by='R2', ascending=False))
 
 else :
 
@@ -127,7 +143,7 @@ else :
     corr = df_train_cla.corr()
 
     fig = plt.figure(figsize=(10, 10))
-    sns.heatmap(corr, annot=True)
+    sns.heatmap(corr, annot=True, cmap='coolwarm')
     st.pyplot(fig)
 
     st.subheader('')
@@ -188,5 +204,52 @@ else :
     plt.ylabel('PC' + str(plan_principaux + 1))
     plt.grid()
     st.pyplot(fig)
+
+    st.subheader('')
+
+    st.markdown(f"<h5 style='text-align: center;'>Résultats de nos modèles</h5>", unsafe_allow_html=True)
+
+    st.subheader('')
+
+    # load every dataframe in the folder résultats_models/classification
+
+    path = 'résultats_models/classif'
+
+    files = os.listdir(path)
+    files.sort()
+    files = [file for file in files if file.endswith('.csv')]
+
+    df = pd.DataFrame({'Modèle': [], 'Accuracy': []})
+
+    for file in files:
+        df_temp = pd.read_csv(os.path.join(path, file))
+        df = df._append({'Modèle': file.split('.')[0].replace('_', ' '), 'Accuracy': accuracy_score(df_temp['y_test'], df_temp['y_pred'])}, ignore_index=True)
+    
+    st.table(df.sort_values(by='Accuracy', ascending=False))
+
+    model = st.sidebar.selectbox('Modèle à tester', files)
+
+    df_temp = pd.read_csv(os.path.join(path, model))
+
+    st.subheader('')
+
+    st.markdown(f"<h5 style='text-align: center;'>Matrice de confusion</h5>", unsafe_allow_html=True)
+
+    st.subheader('')
+
+    fig = plt.figure(figsize=(10, 10))
+    sns.heatmap(confusion_matrix(df_temp['y_test'], df_temp['y_pred']), annot=True, cmap='coolwarm')
+    plt.xlabel('Vraie classe')
+    plt.ylabel('Classe prédite')
+    st.pyplot(fig)
+
+    st.subheader('')
+
+    st.markdown(f"<h5 style='text-align: center;'>Rapport de classification</h5>", unsafe_allow_html=True)
+
+    st.subheader('')
+
+    st.table(pd.DataFrame(classification_report(df_temp['y_test'], df_temp['y_pred'], output_dict=True)).T)
+
 
     
