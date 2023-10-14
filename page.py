859493e6a9733643bb4b8a081bcc1cf22855d838
vilhess <samy.vilhes@gmail.com>
2023-10-14 18:03:30 +0200

    corr streamlit

--- page.py
+++ page.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import os
 from sklearn.metrics import r2_score, accuracy_score, confusion_matrix, classification_report
 
-st.markdown(f"<h1 style='text-align: center;'>Projet Supervisé Avancé</h1>", unsafe_allow_html=True)
+st.markdown(f"<h1 style='text-align: center;'>Projet Supervisé Avancé</h1>",
+            unsafe_allow_html=True)
 st.subheader('')
 st.subheader('')
 
@@ -20,19 +21,22 @@ if probleme == 'Régression':
     df_train_reg = pd.read_csv(data_train_reg_path).drop('wine_ID', axis=1)
     st.table(df_train_reg.sample(5))
 
-    st.markdown(f"<h2 style='text-align: center;'>Description du jeu de données</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='text-align: center;'>Description du jeu de données</h2>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Format du jeu de données : {df_train_reg.shape[0]} lignes et {df_train_reg.shape[1]} variables (dont celle à prédire) </h5>", unsafe_allow_html=True)
+    st.markdown(
+        f"<h5 style='text-align: center;'>Format du jeu de données : {df_train_reg.shape[0]} lignes et {df_train_reg.shape[1]} variables (dont celle à prédire) </h5>", unsafe_allow_html=True)
 
     st.subheader('')
-    
+
     st.table(df_train_reg.describe())
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Corrélation entre les variables</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Corrélation entre les variables</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -44,7 +48,8 @@ if probleme == 'Régression':
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Distribution des variables</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Distribution des variables</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -52,16 +57,20 @@ if probleme == 'Régression':
 
     for i in range(df_train_reg.shape[1]):
         fig.add_subplot(4, 4, i+1)
-        sns.histplot(df_train_reg.iloc[:, i], color='green', label=df_train_reg.columns[i])
-        plt.axvline(df_train_reg.iloc[:, i].mean(), linestyle='dashed', color='red', label='mean')
-        plt.axvline(df_train_reg.iloc[:, i].median(), linestyle='dashed', color='blue', label='median')
+        sns.histplot(
+            df_train_reg.iloc[:, i], color='green', label=df_train_reg.columns[i])
+        plt.axvline(df_train_reg.iloc[:, i].mean(
+        ), linestyle='dashed', color='red', label='mean')
+        plt.axvline(df_train_reg.iloc[:, i].median(
+        ), linestyle='dashed', color='blue', label='median')
         plt.legend()
 
     st.pyplot(fig)
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Analyse en composantes principales</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Analyse en composantes principales</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -70,7 +79,7 @@ if probleme == 'Régression':
 
     fig = plt.figure(figsize=(10, 5))
     plt.plot(range(1, len(pca.explained_variance_ratio_) + 1),
-                pca.explained_variance_ratio_.cumsum())
+             pca.explained_variance_ratio_.cumsum())
     plt.xlabel('Nombre de composantes')
     plt.ylabel('Pourcentage de variance expliqué cumulé')
     plt.grid()
@@ -82,16 +91,18 @@ if probleme == 'Régression':
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Projection des individus sur le plan factoriel {plan_principaux}, {plan_principaux+1} </h5>", unsafe_allow_html=True)
+    st.markdown(
+        f"<h5 style='text-align: center;'>Projection des individus sur le plan factoriel {plan_principaux}, {plan_principaux+1} </h5>", unsafe_allow_html=True)
 
     st.subheader('')
 
     fig = plt.figure(figsize=(10, 10))
 
     new_df = pd.DataFrame(pca.transform(df_train_reg.drop('target', axis=1)), columns=[
-                            'PC' + str(i) for i in range(1, df_train_reg.shape[1])])
+        'PC' + str(i) for i in range(1, df_train_reg.shape[1])])
 
-    sns.scatterplot(x='PC' + str(plan_principaux), y='PC' + str(plan_principaux + 1), data=new_df, hue=df_train_reg['target'], palette='Set3')
+    sns.scatterplot(x='PC' + str(plan_principaux), y='PC' + str(plan_principaux + 1),
+                    data=new_df, hue=df_train_reg['target'], palette='Set3')
     plt.xlabel('PC' + str(plan_principaux))
     plt.ylabel('PC' + str(plan_principaux + 1))
     plt.grid()
@@ -99,7 +110,8 @@ if probleme == 'Régression':
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Résultats de nos modèles</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Résultats de nos modèles</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -113,22 +125,25 @@ if probleme == 'Régression':
 
     for file in files:
         df_temp = pd.read_csv(os.path.join(path, file))
-        df = df._append({'Modèle': file.split('.')[0].replace('_', ' '), 'R2': r2_score(df_temp['y_test'], df_temp['y_pred'])}, ignore_index=True)
+        df = df._append({'Modèle': file.split('.')[0].replace('_', ' '), 'R2': r2_score(
+            df_temp['y_test'], df_temp['y_preds'])}, ignore_index=True)
 
     st.table(df.sort_values(by='R2', ascending=False))
 
-else :
+else:
 
-    data_train_cla_path = 'data/data_cla/stars_train.csv'
+    data_train_cla_path = 'data/data_cla/stars_train_new.csv'
 
     df_train_cla = pd.read_csv(data_train_cla_path).drop('obj_ID', axis=1)
     st.table(df_train_cla.sample(5))
 
-    st.markdown(f"<h2 style='text-align: center;'>Description du jeu de données</h2>", unsafe_allow_html=True)
+    st.markdown(f"<h2 style='text-align: center;'>Description du jeu de données</h2>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Format du jeu de données : {df_train_cla.shape[0]} lignes et {df_train_cla.shape[1]} variables (dont celle à prédire) </h5>", unsafe_allow_html=True)
+    st.markdown(
+        f"<h5 style='text-align: center;'>Format du jeu de données : {df_train_cla.shape[0]} lignes et {df_train_cla.shape[1]} variables (dont celle à prédire) </h5>", unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -136,8 +151,9 @@ else :
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Corrélation entre les variables</h5>", unsafe_allow_html=True)
-    
+    st.markdown(f"<h5 style='text-align: center;'>Corrélation entre les variables</h5>",
+                unsafe_allow_html=True)
+
     st.subheader('')
 
     corr = df_train_cla.corr()
@@ -148,7 +164,8 @@ else :
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Distribution des variables</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Distribution des variables</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -156,16 +173,20 @@ else :
 
     for i in range(df_train_cla.shape[1]):
         fig.add_subplot(4, 4, i+1)
-        sns.histplot(df_train_cla.iloc[:, i], color='green', label=df_train_cla.columns[i])
-        plt.axvline(df_train_cla.iloc[:, i].mean(), linestyle='dashed', color='red', label='mean')
-        plt.axvline(df_train_cla.iloc[:, i].median(), linestyle='dashed', color='blue', label='median')
+        sns.histplot(
+            df_train_cla.iloc[:, i], color='green', label=df_train_cla.columns[i])
+        plt.axvline(df_train_cla.iloc[:, i].mean(
+        ), linestyle='dashed', color='red', label='mean')
+        plt.axvline(df_train_cla.iloc[:, i].median(
+        ), linestyle='dashed', color='blue', label='median')
         plt.legend()
 
     st.pyplot(fig)
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Analyse en composantes principales</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Analyse en composantes principales</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -174,7 +195,7 @@ else :
 
     fig = plt.figure(figsize=(10, 5))
     plt.plot(range(1, len(pca.explained_variance_ratio_) + 1),
-                pca.explained_variance_ratio_.cumsum())
+             pca.explained_variance_ratio_.cumsum())
     plt.xlabel('Nombre de composantes')
     plt.ylabel('Pourcentage de variance expliqué cumulé')
     plt.grid()
@@ -182,7 +203,8 @@ else :
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Plans factoriels</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Plans factoriels</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -190,16 +212,18 @@ else :
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Projection des individus sur le plan factoriel {plan_principaux}</h5>", unsafe_allow_html=True)
+    st.markdown(
+        f"<h5 style='text-align: center;'>Projection des individus sur le plan factoriel {plan_principaux}</h5>", unsafe_allow_html=True)
 
     st.subheader('')
 
     fig = plt.figure(figsize=(10, 10))
 
     new_df = pd.DataFrame(pca.transform(df_train_cla.drop('label', axis=1)), columns=[
-                            'PC' + str(i) for i in range(1, df_train_cla.shape[1])])
-    
-    sns.scatterplot(x='PC' + str(plan_principaux), y='PC' + str(plan_principaux + 1), data=new_df, hue=df_train_cla['label'], palette='Set3')
+        'PC' + str(i) for i in range(1, df_train_cla.shape[1])])
+
+    sns.scatterplot(x='PC' + str(plan_principaux), y='PC' + str(plan_principaux + 1),
+                    data=new_df, hue=df_train_cla['label'], palette='Set3')
     plt.xlabel('PC' + str(plan_principaux))
     plt.ylabel('PC' + str(plan_principaux + 1))
     plt.grid()
@@ -207,7 +231,8 @@ else :
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Résultats de nos modèles</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Résultats de nos modèles</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
@@ -223,8 +248,9 @@ else :
 
     for file in files:
         df_temp = pd.read_csv(os.path.join(path, file))
-        df = df._append({'Modèle': file.split('.')[0].replace('_', ' '), 'Accuracy': accuracy_score(df_temp['y_test'], df_temp['y_pred'])}, ignore_index=True)
-    
+        df = df._append({'Modèle': file.split('.')[0].replace('_', ' '), 'Accuracy': accuracy_score(
+            df_temp['y_test'], df_temp['y_preds'])}, ignore_index=True)
+
     st.table(df.sort_values(by='Accuracy', ascending=False))
 
     model = st.sidebar.selectbox('Modèle à tester', files)
@@ -233,23 +259,24 @@ else :
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Matrice de confusion</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Matrice de confusion</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
     fig = plt.figure(figsize=(10, 10))
-    sns.heatmap(confusion_matrix(df_temp['y_test'], df_temp['y_pred']), annot=True, cmap='coolwarm')
+    sns.heatmap(confusion_matrix(
+        df_temp['y_test'], df_temp['y_preds']), annot=True, cmap='coolwarm')
     plt.xlabel('Vraie classe')
     plt.ylabel('Classe prédite')
     st.pyplot(fig)
 
     st.subheader('')
 
-    st.markdown(f"<h5 style='text-align: center;'>Rapport de classification</h5>", unsafe_allow_html=True)
+    st.markdown(f"<h5 style='text-align: center;'>Rapport de classification</h5>",
+                unsafe_allow_html=True)
 
     st.subheader('')
 
-    st.table(pd.DataFrame(classification_report(df_temp['y_test'], df_temp['y_pred'], output_dict=True)).T)
-
-
-    
+    st.table(pd.DataFrame(classification_report(
+        df_temp['y_test'], df_temp['y_preds'], output_dict=True)).T)
